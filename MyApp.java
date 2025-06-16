2c0c5e0936587c6070e224d1abfa82e4e885ffd2
Wilhelm Lee <wilhelm_lee@dlsu.edu.ph>
2025-06-16 21:43:51 +0800

    Draft Password Method and CRUD Option Menu

--- MyApp.java
+++ MyApp.java
@@ -5,17 +5,17 @@ public class MyApp {
     //Menu strings
     private final String menuString = """
         Welcome to MyApp! Please select an option from the menu below.
-        1. Option 1
-        2. Option 2
-        3. Option 3
+        1. Update Database
+        2. Query Options
+        3. View Reports
 
         (Enter 0 to exit):
         """;
     private final String op1MenuString = """
-        Option 1 Menu:
-        1. Sub-option 1
-        2. Sub-option 2
-        3. Sub-option 3
+        Update Database Menu:
+        1. Create New Entry
+        2. Update Existing Entry
+        3. Delete Entry
 
         (Enter 0 to return to main menu):
         """;
@@ -49,6 +49,12 @@ public class MyApp {
             switch (app.getUserMenuInput()) {
                 case 1:
                     System.out.println("Option 1 selected.");
+                    if (app.correctPassword("dummyPassword")) {
+                        app.updateDatabaseMenu(scanner);
+                    }
+                    else {
+                        System.out.println("Incorrect password. Returning to main menu.");
+                    }
                     break;
                 case 2:
                     System.out.println("Option 2 selected.");
@@ -68,5 +74,41 @@ public class MyApp {
 
         scanner.close();
         System.out.println("Exiting MyApp.");
+    }
+
+    public boolean correctPassword(String password) {
+        //TODO: Password validation logic
+        return true;
+    }
+    //menus
+    private void updateDatabaseMenu(Scanner scanner) {
+        System.out.println(op1MenuString);
+        int op1MenuInput = -1;
+
+        while (op1MenuInput != 0) {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next(); // Clear the invalid input
+            }
+            op1MenuInput = scanner.nextInt();
+
+            switch (op1MenuInput) {
+                case 1:
+                    System.out.println("Creating new entry...");
+                    break;
+                case 2:
+                    System.out.println("Updating existing entry...");
+                    break;
+                case 3:
+                    System.out.println("Deleting entry...");
+                    break;
+                case 0:
+                    System.out.println("Returning to main menu.");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+            }
+        }
     }
 }
