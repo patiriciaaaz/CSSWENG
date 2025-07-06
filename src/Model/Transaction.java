18685771a09ca90fb0bacb6ba54420ef6bf317b6
migsalvador <miguelmanansalasalvador@gmail.com>
2025-07-07 01:12:33 +0800

    Added transaction menu

--- src/Model/Transaction.java
+++ src/Model/Transaction.java
@@ -1,44 +1,53 @@
 package Model;
 
+import java.sql.Date;
+
 public class Transaction {
-    private int itemID;
-    private String itemName;
-    private double price;
-    private int quantity;
+     private int transactionID;
+     private Integer memberID;
+     private Date transactionDate;
+     private double amount;
+     private String transactionType;
 
-    // Getters
-    public int getItemID() {
-         return itemID; 
-    }
-    
-    public String getItemName() {
-         return itemName; 
-    }
-    
-    public double getPrice() {
-         return price; 
-    }
-    
-    public int getQuantity() {
-         return quantity; 
-    }
-    
+     // Getters
+     public int getTransactionID() {
+          return transactionID;
+     }
 
-    // Setters
-    public void setItemID(int itemID) {
-         this.itemID = itemID; 
-    }
-    
-    public void setItemName(String itemName) {
-         this.itemName = itemName; 
-    }
-    
-    public void setPrice(double price) {
-         this.price = price; 
-    }
-    
-    public void setQuantity(int quantity) {
-         this.quantity = quantity; 
-    }
-    
+     public Integer getMemberID() {
+          return memberID;
+     }
+
+     public Date getTransactionDate() {
+          return transactionDate;
+     }
+
+     public double getAmount() {
+          return amount;
+     }
+
+     public String getTransactionType() {
+          return transactionType;
+     }
+
+     // Setters
+     public void setTransactionID(int transactionID) {
+          this.transactionID = transactionID;
+     }
+
+     public void setMemberID(Integer memberID) {
+          this.memberID = memberID;
+     }
+
+     public void setTransactionDate(Date transactionDate) {
+          this.transactionDate = transactionDate;
+     }
+
+     public void setAmount(double amount) {
+          this.amount = amount;
+     }
+
+     public void setTransactionType(String transactionType) {
+          this.transactionType = transactionType;
+     }
 }
